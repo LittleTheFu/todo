f385b32a8a381d9443d6230b58ff73946024ce8c
fuyuxiang <lttlstpd@live.com>
2019-11-04 10:27:25 +0800

    add a route assosiate with param

--- app/main/views.py
+++ app/main/views.py
@@ -3,8 +3,12 @@ from . import main
 
 @main.route('/')
 def index():
-    return  render_template('index.html')
+    return  render_template( 'index.html' )
 
 @main.route('/hello')
 def hello():
-    return render_template('hello.html')
+    return render_template( 'hello.html' )
+
+@main.route('/names/<name>')
+def name( name ):
+    return render_template( 'name.html', name = name )
