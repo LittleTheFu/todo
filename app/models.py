7ee633e4914497bf20107222c97a1fdd76851e59
fuyuxiang <lttlstpd@live.com>
2019-11-05 15:16:12 +0800

    display create time of each todo

--- app/models.py
+++ app/models.py
@@ -1,14 +1,19 @@
 from . import db
+from datetime import datetime
 
 class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
+    create_date = db.Column(db.Date)
 
     @staticmethod
     def add(name):
         todo = Todo()
         todo.name = name
+        todo.create_date = datetime.utcnow()
+        print("@@@@@@@@@@@@@@@@@@")
+        print(todo.create_date)
         db.session.add(todo)
         db.session.commit()
 
