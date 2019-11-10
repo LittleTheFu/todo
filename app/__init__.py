3e85738f2659c8544327e942a602c3223de074a9
fuyuxiang <lttlstpd@live.com>
2019-11-10 20:28:55 +0800

    integrate with flask-avatars

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from config import config
 from flask_moment import Moment
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_avatars import Avatars
 
 
 
@@ -12,7 +13,7 @@ db = SQLAlchemy()
 bootstrap = Bootstrap()
 moment = Moment()
 mail = Mail()
-
+avatars = Avatars()
 
 login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
@@ -27,6 +28,7 @@ def create_app(config_name):
     moment.init_app(app)
     login_manager.init_app(app)
     mail.init_app(app)
+    avatars.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
