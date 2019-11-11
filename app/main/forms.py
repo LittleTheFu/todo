c9b7f4f4d967d832e2caac6604562d042d34cd11
fuyuxiang <lttlstpd@live.com>
2019-11-11 15:05:19 +0800

    remove cancel button

--- app/main/forms.py
+++ app/main/forms.py
@@ -8,8 +8,9 @@ class NameForm(FlaskForm):
 class TodoEditForm(FlaskForm):
     name = StringField('name : ')
     submit = SubmitField('Submit')
+    # cancel = SubmitField('Cancel')
 
 class EditProfileForm(FlaskForm):
     about_me = StringField('about me : ')
     submit = SubmitField('Submit')
-    cancel = SubmitField('Cancel')
+    # cancel = SubmitField('Cancel')
